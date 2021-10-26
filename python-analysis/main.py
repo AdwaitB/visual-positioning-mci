303f1a1f813d3e60ca709aeca3bf9ff2feb81d6a
Adwait Bauskar <AdwaitB@users.noreply.github.com>
2021-10-26 17:56:17 -0400

    sift basic setup and example clough image

--- python-analysis/main.py
+++ python-analysis/main.py
@@ -1,5 +1,16 @@
 import cv2
 
-img = cv2.imread('example.jpeg')
 
-cv2.imwrite('out.jpeg', img)
+def getFeaturePoints(path):
+    img = cv2.imread(path + ".jpeg")
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Applying SIFT detector
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp = sift.detect(img, None)
+
+    img = cv2.drawKeypoints(img, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imwrite(path + "-fp.jpeg", img)
+
+
+getFeaturePoints("./images/clough/1/1")
+getFeaturePoints("./images/clough/1/2")
