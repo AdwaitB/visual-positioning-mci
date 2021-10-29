a63f494903d0d7d2f0916b22e4edc52d7f49fa6c
Sankalp Sangle <sankalpsangle@Sankalps-MacBook-Air.local>
2021-10-28 21:54:36 -0400

    Added img folder and modified analysis script

--- python-analysis/main.py
+++ python-analysis/main.py
@@ -1,16 +1,40 @@
 import cv2
 
+BUILDINGS   = ['CLOUGH']
+ANGLES      = [1,2,3,4,5,6,7]
+TIME_OF_DAY = [1,2]
 
-def getFeaturePoints(path):
-    img = cv2.imread(path + ".jpeg")
+PREFIX_FOLDER = "img/"
+
+def generateImgPaths():
+    ans = []
+    
+    for building in BUILDINGS:
+        for angle in ANGLES:
+            for time in TIME_OF_DAY:
+                ans.append(building + "-" + str(angle) + "-" + str(time))
+    
+    return ans
+
+def generateFeaturePointsToFile(path):
+    img = cv2.imread(path + ".jpg")
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Applying SIFT detector
     sift = cv2.xfeatures2d.SIFT_create()
     kp = sift.detect(img, None)
 
     img = cv2.drawKeypoints(img, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imwrite(path + "-fp.jpeg", img)
+    cv2.imwrite(path + "-fp.jpg", img)
 
 
-getFeaturePoints("./images/clough/1/1")
-getFeaturePoints("./images/clough/1/2")
+def main():
+
+    IMG_PATHS = generateImgPaths()
+
+    IMG_PATHS = [PREFIX_FOLDER + x for x in IMG_PATHS]
+
+    for image_path in IMG_PATHS:
+        generateFeaturePointsToFile(image_path)
+
+if __name__ == "__main__":
+    main()
